c58159a3bee2ca43e262b132272216ce6bb92b30
Daniel <oladotun.olufemi@payhippo.ng>
2024-01-21 01:32:47 +0100

    update team

--- src/app/_utils/teams.jsx
+++ src/app/_utils/teams.jsx
@@ -1,60 +1,60 @@
 // Image
-import TeamOne from '@/_assets/images/team-one.png';
-import TeamTwo from '@/_assets/images/team-two.png';
-import TeamThree from '@/_assets/images/team-three.png';
-import TeamFour from '@/_assets/images/team-four.png';
-import TeamFive from '@/_assets/images/team-five.png';
-import TeamSix from '@/_assets/images/team-six.png';
-import TeamSeven from '@/_assets/images/team-seven.png';
-import TeamEight from '@/_assets/images/team-eight.png';
+import TeamEight from "@/_assets/images/team-eight.png";
+import TeamFive from "@/_assets/images/team-five.png";
+import TeamFour from "@/_assets/images/team-four.png";
+import TeamOne from "@/_assets/images/team-one.png";
+import TeamSeven from "@/_assets/images/team-seven.png";
+import TeamSix from "@/_assets/images/team-six.png";
+import TeamThree from "@/_assets/images/team-three.png";
+import TeamTwo from "@/_assets/images/team-two.jpg";
 
 export const teams = [
   {
     id: 1,
-    name: 'anthony ogunjinrin',
-    role: 'founder',
+    name: "anthony ogunjinrin",
+    role: "founder",
     image: TeamOne,
   },
   {
     id: 2,
-    name: 'tomide adeloye',
-    role: 'founder',
+    name: "MR. TA",
+    role: "MD/Co-Founder",
     image: TeamTwo,
   },
   {
     id: 3,
-    name: 'abiola ayomide',
-    role: 'co-founder',
+    name: "abiola ayomide",
+    role: "co-founder",
     image: TeamThree,
   },
   {
     id: 4,
-    name: 'shittu mubarak B',
-    role: 'coo',
+    name: "shittu mubarak B",
+    role: "coo",
     image: TeamFour,
   },
   {
     id: 5,
-    name: 'olaitan odufuwa',
-    role: 'cto',
+    name: "olaitan odufuwa",
+    role: "cto",
     image: TeamFive,
   },
   {
     id: 6,
-    name: 'adetola ogunkunle',
-    role: 'lead coach',
+    name: "adetola ogunkunle",
+    role: "lead coach",
     image: TeamSix,
   },
   {
     id: 7,
-    name: 'shola ibrahim',
-    role: 'lead coach II',
+    name: "shola ibrahim",
+    role: "lead coach II",
     image: TeamSeven,
   },
   {
     id: 8,
-    name: 'azubuike chisom',
-    role: 'customer service specialist',
+    name: "azubuike chisom",
+    role: "customer service specialist",
     image: TeamEight,
   },
 ];
